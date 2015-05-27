48a9c15b5c67e52de964ebabb067b6fb20658e28
yukung <yukung.i@gmail.com>
2015-05-27 15:37:29 +0900

    Use the 'Bootstrap' and 'Font-Awesome'.

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -22,6 +22,7 @@
 // to create separate JavaScript files as needed.
 //
 //= require jquery-2.1.3.js
+//= require webjars/bootstrap/3.3.4/dist/js/bootstrap.js
 //= require_tree .
 //= require_self
 
